300b6aac996a485bfd09956c581b36aa543dfed8
Tatiana Shuliak <tatjananikolaevich23041997@gmail.com>
2023-03-13 20:40:18 +0300

    Init commit

---
+++ Homework6/Calculator/src/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+    double firstVariable;
+    double secondVariable;
+
+    public Calculator(double firstVariable, double secondVariable) {
+        this.firstVariable = firstVariable;
+        this.secondVariable = secondVariable;
+    }
+
+    public void calculateSum() {
+        System.out.println("Сумма двух чисел равна " + (firstVariable + secondVariable));
+    }
+
+    public void calculateDif() {
+        System.out.println("Разность двух чисел равна " + (firstVariable - secondVariable));
+    }
+
+    public void calculateMult() {
+        System.out.println("Произведение двух чисел равно " + (firstVariable * secondVariable));
+    }
+
+    public void calculateDiv() {
+        System.out.println("Частное двух чисел равно " + (firstVariable / secondVariable));
+    }
+}
---
+++ Homework6/Computer/src/Computer.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Computer {
+    String processor;
+    int ram;
+    int hardDisk;
+    int cyclesOfWork;
+    boolean computerIsWorking;
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+    public Computer(String processor, int ram, int hardDisk, int cyclesOfWork, boolean computerIsWorking) {
+        this.processor = processor;
+        this.ram = ram;
+        this.hardDisk = hardDisk;
+        this.cyclesOfWork = cyclesOfWork;
+        this.computerIsWorking = computerIsWorking;
+    }
+
+    public void describe() {
+        System.out.println("Процессор компьютера - " + processor);
+        System.out.println("Оперативная память компьютера - " + ram + " гб");
+        System.out.println("Жесткий диск компьютера - " + hardDisk + " гб");
+        System.out.println("Ресурс полных циклов работы - " + cyclesOfWork);
+    }
+
+    public void turnOn() {
+            int a = random.nextInt(2);
+            System.out.println("Чтобы включить компьютер, введите число 0 или 1");
+            int b = scanner.nextInt();
+            if(computerIsWorking){
+            if (a == b) {
+                System.out.println("Идет включение компьютера...");
+            } else if (b != 0 && b != 1) {
+                System.out.println("Вы выбрали несуществующую опцию");
+                computerIsWorking = false;
+            } else {
+                System.out.println("К сожалению, Ваш компьютер сгорел");
+                computerIsWorking = false;
+            }
+        }
+        else{
+            System.out.println("Невозможно включить компьютер, он сгорел");
+        }
+
+    }
+
+        public void turnOff() {
+            if (computerIsWorking) {
+                int a = random.nextInt(2);
+                System.out.println("Чтобы выключить компьютер, введите число 0 или 1");
+                int b = scanner.nextInt();
+                if (a == b) {
+                    System.out.println("Идет выключение компьютера...");
+                } else if (b != 0 && b != 1) {
+                    System.out.println("Вы выбрали несуществующую опцию");
+                    computerIsWorking = false;
+                } else {
+                    System.out.println("К сожалению, Ваш компьютер сгорел");
+                    computerIsWorking = false;
+                }
+            }
+        }
+    }
